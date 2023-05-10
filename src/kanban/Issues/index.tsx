04b20ab3a7736684b1b66477590f086328bd98f8
miaoyu <miaoyu@ifun.com>
2023-05-10 18:01:33 +0800

    feat: add drop area

--- src/kanban/Issues/index.tsx
+++ src/kanban/Issues/index.tsx
@@ -20,12 +20,22 @@ const DropArea = styled.div`
   padding: 15px 15px;
 `;
 
+const DropStatusArea = styled.div`
+  width: 100%;
+  height: 100px;
+  box-sizing: border-box;
+  border: 1px solid purple;
+  &:hover {
+    border: 1px solid green;
+  }
+`;
 const Issues: React.FC<IssuesProps> = (props) => {
   const { issues, groupId } = props;
   return (
     <Droppable key={issues.id} droppableId={handleId(groupId, issues.id)}>
       {(provided, snapshot) => (
         <DropArea ref={provided.innerRef} {...provided.droppableProps}>
+          <DropStatusArea>123</DropStatusArea>
           {issues.list?.map((item, index) => (
             <IssueCard
               groupId={groupId}
